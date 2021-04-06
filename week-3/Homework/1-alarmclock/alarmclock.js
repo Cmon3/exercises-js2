0ae1899a63ecc7bbcaf0822f04b9e56c8b3a4c3d
Cmon3 <cmonlg@gmail.com>
2021-04-06 20:24:00 +0200

    homework

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,4 +1,37 @@
-function setAlarm() {}
+/*let setTime = document.querySelector("#set");
+let timeRe = document.querySelector("#timeRemaining");
+
+setTime.addEventListener("click", function() {
+  const myInsertText = document.querySelector("#alarmSet");
+  //myInsertText.innerText = setTime;
+  timeRe.innerText(myInsertText);
+});*/
+
+function setAlarm() {
+  let alarmTime = document.querySelector("#alarmSet").valueAsNumber;
+
+  let text = document.querySelector("#timeRemaining");
+
+  let currentTime = alarmTime;
+
+  timerID = setInterval(
+    function (num) {
+      if (currentTime === 0) {
+        clearInterval(timerID);
+        //let bodyColor = document.querySelector("body");
+        document.body.style.backgroundColor = "pink";
+        playAlarm();
+      }
+      if (currentTime >= 0) {
+        text.innerText = "Time Remaining: 00:" + currentTime;
+        console.log(currentTime);
+      }
+      currentTime = currentTime - 1; 
+    }, 
+    1000, 
+    alarmTime
+  );
+}
 
 // DO NOT EDIT BELOW HERE
 
